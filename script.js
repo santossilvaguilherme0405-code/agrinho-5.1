86358a3121c659987fa84b49dadda90950031112
Guilherme dos Santos Silva <santos.silva.guilherme0405@escola.pr.gov.br>
2026-05-06 10:10:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-window.addEventListener("scroll", function() {
-document.querySelectorAll('.fade').forEach(el => {
-if (el.getBoundingClientRect().top < window.innerHeight - 50) {
-el.classList.add('show');
-}
-});
-});
-
-function scrollToSection(id) {
-document.getElementById(id).scrollIntoView({
-behavior: "smooth"
-});
-}
-
-function zoomImg(img) {
+function abrirZoom(img) {
 document.getElementById("modal").style.display = "flex";
 document.getElementById("imgZoom").src = img.src;
 }
@@ -20,37 +6,3 @@ document.getElementById("imgZoom").src = img.src;
 function fecharZoom() {
 document.getElementById("modal").style.display = "none";
 }
-
-// Upload de imagem
-const upload = document.getElementById("upload");
-const galeria = document.getElementById("galeriaContainer");
-
-upload.addEventListener("change", function() {
-const file = this.files[0];
-if (file) {
-const reader = new FileReader();
-reader.onload = function(e) {
-const img = document.createElement("img");
-img.src = e.target.result;
-img.onclick = function() { zoomImg(img); };
-galeria.appendChild(img);
-};
-reader.readAsDataURL(file);
-}
-});
-
-// contador
-function animateValue(id, end) {
-let start = 0;
-let interval = setInterval(() => {
-start++;
-document.getElementById(id).innerText = start + "%";
-if (start >= end) clearInterval(interval);
-}, 15);
-}
-
-window.onload = () => {
-animateValue("num1", 95);
-animateValue("num2", 90);
-animateValue("num3", 98);
-};
