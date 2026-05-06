bcd820420c9adc0f4b6cca0d936973bff9942b4c
Guilherme dos Santos Silva <santos.silva.guilherme0405@escola.pr.gov.br>
2026-05-06 20:52:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,58 +6,84 @@ let index = 0;
 const modal = document.getElementById("modal");
 const imgZoom = document.getElementById("imgZoom");
 
-// abrir zoom
-imagens.forEach((img, i) => {
-img.addEventListener("click", () => {
-index = i;
-modal.style.display = "flex";
-imgZoom.src = img.src;
-});
-});
-
-// fechar
-document.querySelector(".fechar").onclick = () => {
-modal.style.display = "none";
-};
-
-// trocar
-document.querySelector(".prev").onclick = () => {
-index = (index - 1 + imagens.length) % imagens.length;
-imgZoom.src = imagens[index].src;
-};
-
-document.querySelector(".next").onclick = () => {
-index = (index + 1) % imagens.length;
-imgZoom.src = imagens[index].src;
-};
-
-// upload
-const upload = document.getElementById("upload");
-const galeria = document.getElementById("galeriaContainer");
-
-upload.addEventListener("change", function() {
-const file = this.files[0];
-if (file) {
-const reader = new FileReader();
-reader.onload = function(e) {
-const img = document.createElement("img");
-img.src = e.target.result;
-
-```
-    img.addEventListener("click", () => {
-      imagens = document.querySelectorAll(".galeria img");
-      index = imagens.length - 1;
-      modal.style.display = "flex";
-      imgZoom.src = img.src;
-    });
-
-    galeria.appendChild(img);
-    imagens = document.querySelectorAll(".galeria img");
-  };
-  reader.readAsDataURL(file);
+function atualizarLista(){
+imagens = document.querySelectorAll(".galeria img");
 }
-```
 
+imagens.forEach((img,i)=>{
+img.onclick = ()=>{
+index=i;
+modal.style.display="flex";
+imgZoom.src=img.src;
+};
 });
 
+document.querySelector(".fechar").onclick=()=>modal.style.display="none";
+
+document.querySelector(".next").onclick=()=>{
+index=(index+1)%imagens.length;
+imgZoom.src=imagens[index].src;
+};
+
+document.querySelector(".prev").onclick=()=>{
+index=(index-1+imagens.length)%imagens.length;
+imgZoom.src=imagens[index].src;
+};
+
+document.addEventListener("keydown",(e)=>{
+if(e.key==="ArrowRight") document.querySelector(".next").click();
+if(e.key==="ArrowLeft") document.querySelector(".prev").click();
+if(e.key==="Escape") modal.style.display="none";
+});
+
+// UPLOAD + DRAG
+const upload=document.getElementById("upload");
+const galeria=document.getElementById("galeriaContainer");
+const drop=document.getElementById("dropArea");
+
+function addImg(src){
+const img=document.createElement("img");
+img.src=src;
+img.onclick=()=>{
+atualizarLista();
+index=imagens.length-1;
+modal.style.display="flex";
+imgZoom.src=src;
+};
+galeria.appendChild(img);
+atualizarLista();
+}
+
+upload.onchange=function(){
+[...this.files].forEach(file=>{
+const reader=new FileReader();
+reader.onload=e=>addImg(e.target.result);
+reader.readAsDataURL(file);
+});
+};
+
+drop.ondragover=e=>{e.preventDefault();};
+drop.ondrop=e=>{
+e.preventDefault();
+[...e.dataTransfer.files].forEach(file=>{
+const reader=new FileReader();
+reader.onload=e=>addImg(e.target.result);
+reader.readAsDataURL(file);
+});
+};
+
+// CONTADOR
+function animar(id,final){
+let n=0;
+let el=document.getElementById(id);
+let i=setInterval(()=>{
+n++;
+el.innerText=n+"%";
+if(n>=final) clearInterval(i);
+},20);
+}
+
+animar("n1",95);
+animar("n2",90);
+animar("n3",98);
 };
