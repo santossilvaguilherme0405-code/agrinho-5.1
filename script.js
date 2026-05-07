f31dd4d8a60d24800e9062c721484b6ef5261345
Guilherme dos Santos Silva <santos.silva.guilherme0405@escola.pr.gov.br>
2026-05-06 21:34:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,105 +1,18 @@
 window.onload = function(){
 
-// LOGIN
 const entrar = document.getElementById("entrar");
 
 const loginScreen = document.getElementById("loginScreen");
 
 const site = document.getElementById("site");
 
-// esconder site
 site.style.display = "none";
 
-// botão entrar
 entrar.onclick = function(){
 
-loginScreen.style.display = "none";
+  loginScreen.style.display = "none";
 
-site.style.display = "block";
-
-};
-
-// GEOLOCALIZAÇÃO
-navigator.geolocation.getCurrentPosition(
-
-function(position){
-
-document.getElementById("cidade").innerText =
-"Localização ativada";
-
-let temp = Math.floor(Math.random()*10)+22;
-
-let umi = Math.floor(Math.random()*30)+60;
-
-document.getElementById("temperatura").innerText =
-temp + "°C";
-
-document.getElementById("umidade").innerText =
-umi + "%";
-
-},
-
-function(){
-
-document.getElementById("cidade").innerText =
-"Localização negada";
-
-}
-
-);
-
-// ZOOM
-let imagens = document.querySelectorAll(".galeria img");
-
-let index = 0;
-
-const modal = document.getElementById("modal");
-
-const imgZoom = document.getElementById("imgZoom");
-
-imagens.forEach((img,i)=>{
-
-img.onclick = ()=>{
-
-```
-index = i;
-
-modal.style.display = "flex";
-
-imgZoom.src = imagens[index].src;
-```
-
-};
-
-});
-
-document.querySelector(".fechar").onclick = ()=>{
-
-modal.style.display = "none";
-
-};
-
-document.querySelector(".next").onclick = ()=>{
-
-index++;
-
-if(index >= imagens.length){
-index = 0;
-}
-
-imgZoom.src = imagens[index].src;
-
-};
-
-document.querySelector(".prev").onclick = ()=>{
-
-index--;
-
-if(index < 0){
-index = imagens.length - 1;
-}
-
-imgZoom.src = imagens[index].src;
+  site.style.display = "block";
 
 };
 
